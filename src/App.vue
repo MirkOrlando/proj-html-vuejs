368dbf0c9df7d9f7344425ded5338806d23b6276
MirkOrlando <mirko@orlando-m.it>
2022-05-13 17:18:28 +0200

    create left menu .footer-top

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,29 @@
       <div class="footer-top py-3">
         <div class="container">
           <div class="row">
-            <div class="col-3">LEFT</div>
+            <div class="col-3">
+              <div class="logo">
+                <img src="@/assets/img/logo-200x34.png" alt="logo" />
+              </div>
+              <p class="py-2">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima
+                sunt iure similique suscipit quasi. Et, recusandae.
+                Voluptatibus, perferendis ab aperiam.
+              </p>
+              <div class="social">
+                <font-awesome-icon
+                  class="icon"
+                  icon="fa-brands fa-facebook-f"
+                />
+                <font-awesome-icon class="icon" icon="fa-brands fa-twitter" />
+                <font-awesome-icon class="icon" icon="fa-brands fa-youtube" />
+                <font-awesome-icon class="icon" icon="fa-brands fa-instagram" />
+                <font-awesome-icon
+                  class="icon"
+                  icon="fa-brands fa-linkedin-in"
+                />
+              </div>
+            </div>
             <div class="col-3">CENTER</div>
             <div class="col-3">RIGHT</div>
           </div>
@@ -31,8 +53,15 @@ export default {
 footer {
   background-color: $darkestColor;
   .footer-top {
-    color: $lightestColor;
+    color: $jumbo;
     text-align: center;
+    .social {
+      color: $socialIconColor /* ff414b */;
+      .icon {
+        margin: 0 0.25rem;
+        padding: 0 0.25rem;
+      }
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,12 +7,18 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 /* import specific icons */
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faYoutube } from '@fortawesome/free-brands-svg-icons'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
 
 /* import font awesome icon component */
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* add icons to the library */
-library.add(faCartShopping, faMagnifyingGlass)
+library.add(faCartShopping, faMagnifyingGlass, faFacebookF, faTwitter, faYoutube, faInstagram, faLinkedinIn)
 
 /* add font awesome icon component */
 Vue.component('font-awesome-icon', FontAwesomeIcon)
