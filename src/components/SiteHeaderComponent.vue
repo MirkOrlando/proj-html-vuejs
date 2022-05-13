a5a805f74118cde619a8d4ef1ae953df9e9c1a79
MirkOrlando <mirko@orlando-m.it>
2022-05-13 16:34:11 +0200

    create SiteHeaderComponent

---
+++ src/components/SiteHeaderComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <header>
+    <div class="container">
+      <nav>
+        <div class="logo">
+          <img src="@/assets/img/logo.png" alt="logo" />
+        </div>
+        <div class="nav-menu">
+          <ul class="nav-items">
+            <li
+              v-for="(navItem, index) in navItems"
+              :key="index"
+              :class="getNavItemClass(index)"
+              @click="getActiveNavItem(index)"
+            >
+              <a href="#">{{ navItem }}</a>
+            </li>
+          </ul>
+          <div class="nav-actions">
+            <a href="#" class="btn btn-primary">Schedule a Workout</a>
+            <a href="#">
+              <font-awesome-icon
+                icon="fa-solid fa-cart-shopping"
+                class="mx-2"
+              />
+            </a>
+            <a href="#">
+              <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+            </a>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SiteHeaderComponent",
+  data() {
+    return {
+      navItems: ["Home", "Services", "About", "Videos", "Blog", "Store"],
+      activeNavItem: 0,
+    };
+  },
+  methods: {
+    getActiveNavItem(i) {
+      this.activeNavItem = i;
+    },
+    getNavItemClass(i) {
+      if (this.navItems[i] === "Store" && this.activeNavItem === i) {
+        return "active new";
+      } else if (this.navItems[i] === "Store") {
+        return "new";
+      } else if (this.activeNavItem === i) {
+        return "active";
+      } else {
+        return "";
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  background-color: $darkestColor;
+  color: $jumbo;
+  padding: 2rem 0;
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .nav-menu {
+      display: flex;
+      gap: 2rem;
+      width: fit-content;
+      .nav-items {
+        display: flex;
+        li {
+          display: flex;
+          padding: 0 2rem;
+          &.new a:after {
+            content: "new";
+            font-size: 0.75rem;
+            color: $darkestColor;
+            margin-left: 0.5rem;
+            padding: 0.5rem;
+            background-color: $new;
+          }
+          &.active {
+            color: $lightestColor;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
