ccacbe4298603657bc808aec532f550089c593e3
MirkOrlando <mirko@orlando-m.it>
2022-05-15 12:55:53 +0200

    create section .jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <SiteHeader />
-    <main>Site Main Here</main>
+    <main>
+      <section class="jumbotron">
+        <div class="container"></div>
+      </section>
+    </main>
     <SiteFooter />
   </div>
 </template>
@@ -20,4 +24,12 @@ export default {
 
 <style lang="scss">
 @import "@/assets/scss/style.scss";
+.jumbotron {
+  height: 70vh;
+  background: linear-gradient(to right, #060606, #060606 45%, transparent 76%),
+    url(http://localhost:8080/img/slider82x-scaled.eb2b1d9f.jpg);
+  background-size: auto, cover;
+  background-position: top right;
+  background-repeat: no-repeat;
+}
 </style>
