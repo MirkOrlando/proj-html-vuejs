e1a9ec07d7590c0156dd0c2d921d21cbf0d45ec2
MirkOrlando <mirko@orlando-m.it>
2022-05-16 12:09:38 +0200

    fix css .videos .featured-playlist

--- src/App.vue
+++ src/App.vue
@@ -115,16 +115,23 @@
           <div class="container">
             <div class="top">
               <h4>Featured playlist</h4>
-              <p>
+              <div class="all">
                 View all videos
                 <font-awesome-icon icon="fa-solid fa-chevron-right" />
-              </p>
+              </div>
             </div>
-            <div class="row">
+            <div class="row g-2">
               <div class="col-4" v-for="(video, index) in videos" :key="index">
-                <img :src="video.src" alt="" />
-                <h5>{{ video.title }}</h5>
-                <p>{{ video.text }}</p>
+                <div class="card">
+                  <div class="card-video p-relative">
+                    <img :src="video.src" alt="" />
+                    <PlayButton />
+                  </div>
+                </div>
+                <div class="card-details">
+                  <h5>{{ video.title }}</h5>
+                  <p class="card-text">{{ video.text }}</p>
+                </div>
               </div>
             </div>
           </div>
@@ -358,6 +365,38 @@ export default {
     }
     .action {
       margin-top: 3rem;
+    }
+  }
+  .featured-playlist {
+    .top {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 2rem 0;
+      h4 {
+        font-size: 1.5rem;
+      }
+      .all .fa-chevron-right {
+        font-size: 0.75rem;
+      }
+    }
+    .card {
+      .card-video,
+      img {
+        border-bottom-right-radius: 2rem;
+      }
+    }
+    .card-details {
+      text-align: center;
+      margin-top: 1.5rem;
+      h5 {
+        font-size: 1rem;
+        margin: 0.5rem 0;
+      }
+      .card-text {
+        color: $primaryColor;
+        font-size: 0.75rem;
+      }
     }
   }
 }
