8b8dbb98863cbdc8b0c5b5bfc9dccc51bf2962c3
MirkOrlando <mirko@orlando-m.it>
2022-05-17 16:36:53 +0200

    create CardVideoComponent

---
+++ src/components/CardVideoComponent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="col-4 col-sm-6 col-xs">
+    <div class="card">
+      <div class="card-video p-relative">
+        <img :src="video.src" alt="" />
+        <PlayButton />
+      </div>
+      <div class="card-details">
+        <h5>{{ video.title }}</h5>
+        <p class="card-text">{{ video.text }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlayButton from "@/components/PlayButtonComponent.vue";
+export default {
+  name: "CardVideoComponent",
+  props: {
+    video: Object,
+  },
+  components: {
+    PlayButton,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  .card-video,
+  img {
+    border-bottom-right-radius: 2rem;
+  }
+  .card-details {
+    text-align: center;
+    margin-top: 1.5rem;
+    h5 {
+      font-size: 1.25em;
+      margin: 0.5rem 0;
+    }
+    .card-text {
+      color: $primaryColor;
+    }
+  }
+}
+</style>
--- src/components/FeaturedPlaylistComponent.vue
+++ src/components/FeaturedPlaylistComponent.vue
@@ -9,33 +9,22 @@
         </div>
       </div>
       <div class="row row-sm">
-        <div
-          class="col-4 col-sm-6 col-xs"
+        <CardVideo
+          :video="video"
           v-for="(video, index) in videos"
           :key="index"
-        >
-          <div class="card">
-            <div class="card-video p-relative">
-              <img :src="video.src" alt="" />
-              <PlayButton />
-            </div>
-            <div class="card-details">
-              <h5>{{ video.title }}</h5>
-              <p class="card-text">{{ video.text }}</p>
-            </div>
-          </div>
-        </div>
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import PlayButton from "@/components/PlayButtonComponent.vue";
+import CardVideo from "@/components/CardVideoComponent.vue";
 export default {
   name: "FeaturedPlaylistComponent",
   components: {
-    PlayButton,
+    CardVideo,
   },
   data() {
     return {
@@ -73,23 +62,6 @@ export default {
     }
     .all .fa-chevron-right {
       font-size: 0.75rem;
-    }
-  }
-  .card {
-    .card-video,
-    img {
-      border-bottom-right-radius: 2rem;
-    }
-    .card-details {
-      text-align: center;
-      margin-top: 1.5rem;
-      h5 {
-        font-size: 1.25em;
-        margin: 0.5rem 0;
-      }
-      .card-text {
-        color: $primaryColor;
-      }
     }
   }
 }
